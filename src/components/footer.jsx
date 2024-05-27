e87428b545749a40df811e627c1bb0e14d082b61
vinay kumar maurya <113638115+vinne01@users.noreply.github.com>
2024-05-27 22:42:27 +0530

    DSA Projects

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -6,9 +6,9 @@ const Footer = () => {
         <footer>
             <p>
                 Created with <i className="fa fa-heart" style={{color: "#FF5252"}}></i> by
-                <a href="https://www.linkedin.com/in/mansimar-anand/" target="_blank" rel="noopener noreferrer" className="link">
+                <a href="https://www.linkedin.com/in/vinaymry/" target="_blank" rel="noopener noreferrer" className="link">
                     <span> </span>
-                    Mansimar Anand
+                    Vinay kumar Maurya
                 </a>
             </p>
         </footer>
